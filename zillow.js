ce8257b91723365bc2bfe914070c93a2632c93b8
Alex Saveliev <alexvsaveliev@gmail.com>
2020-08-09 11:23:08 -0400

    getting data now

--- zillow.js
+++ zillow.js
@@ -21,7 +21,6 @@ const city = 'dover-nh';
 
 // puppeteer usage as normal
 puppeteer.launch({ headless: false, defaultViewport: null, args:['--start-maximized']}).then(async browser => {
-    console.log('Running tests..')
     const page = await browser.newPage();
     await page.setViewport({width: 1920, height: 1080});
 
@@ -31,6 +30,19 @@ puppeteer.launch({ headless: false, defaultViewport: null, args:['--start-maximi
     //.list-card to get all the property cards
     const cards = await page.$$('.list-card');
     //console.log(cards[0]);
+
+    //next part just clicks through, 
+    //this is what actually saves stuff. 
+    page.on('response', async res => {
+        //exclude walk & transit for now, I guess
+        if(res.url().startsWith('https://www.zillow.com/graphql/') && !res.url().includes('WalkAndTransitScoreQuery')){
+            //console.log(res.url());
+            const json = await res.json();
+            //this right here has a good amount of data
+            //console.log(json.data.property);
+        
+        }
+    });
 
     //potentially randomize order
     //or just add a wait
