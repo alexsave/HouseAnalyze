e8c921170c5083021837f55dca4ac481543325cd
Alex Saveliev <alexvsaveliev@gmail.com>
2020-08-09 10:49:29 -0400

    got one element, it's all over for zillow

--- zillow.js
+++ zillow.js
@@ -11,4 +11,34 @@
 //best way might be to do this with puppeteer
 
 
+const puppeteer = require('puppeteer-extra')
+
+// add stealth plugin and use defaults (all evasion techniques)
+    const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+const city = 'dover-nh';
+
+// puppeteer usage as normal
+puppeteer.launch({ headless: false, defaultViewport: null, args:['--start-maximized']}).then(async browser => {
+    console.log('Running tests..')
+    const page = await browser.newPage();
+    await page.setViewport({width: 1920, height: 1080});
+
+    await page.goto(`https://www.zillow.com/${city}/sold/`, {waitUntil: 'networkidle0'});
+    await page.waitFor(5000)
+
+    //.list-card to get all the property cards
+    const cards = await page.$$('.list-card');
+    console.log(cards[0]);
+
+    
+    await browser.close()
+})
+
+
+//when you load all the sold houses, save the time. 
+//then from then on, use the time filter to save time scraping
+//it's not like sold data changes
+
 
