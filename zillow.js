4b275779275332754e27dd1ec965b1d3e6b7a792
Alex Saveliev <alexvsaveliev@gmail.com>
2020-08-09 10:23:30 -0400

    added puppeteer

---
+++ zillow.js
@@ -0,0 +1,14 @@
+// first step is to download a bunch of data
+// we will look at house sale prices and try to fit a model
+// then look at rent in the area and run it on the same houses to predict rent
+// ??
+// profit
+
+//Starting small with Dover NH
+//there are many house listing things but zillow sounds fine to start with
+//starting with sold
+
+//best way might be to do this with puppeteer
+
+
+
