baa1aa80daa83618f8fb17730d373dcf1cbc3b8c
Alex Saveliev <alexvsaveliev@gmail.com>
2020-08-09 16:07:17 -0400

    works for one load

--- zillow.js
+++ zillow.js
@@ -35,35 +35,34 @@ let page;
 //much easier way to do it potentially
 //use the browser to directly load urls rather than clicking and stuff
 const loadPages = async (page) => {
-    console.log(baseURL);
 
-    //get rid of everything after '&wants=' to get better results
+    //you can't win, zillow
+    baseURL = baseURL.replace('includeList=false', 'includeList=true');
+    if(baseURL.includes("&wants="))
+        baseURL = baseURL.substring(0, baseURL.lastIndexOf("&wants=") + 1);
+
+    console.log(baseURL);
 
     
     //better to do it in browser, https fetch is unreliable
     await page.goto(baseURL, {waitUntil: 'networkidle0'});
-    await page.waitFor(5000);
+    //await page.waitFor(5000);
 
-    /*https.get(baseURL, res => {
-        let data = '';
+    //now we parse
+    const elem = await page.$('pre');
+    const raw = await (await elem.getProperty('textContent')).jsonValue();
+    console.log(raw);
 
-        // A chunk of data has been recieved.
-        res.on('data', (chunk) => 
-            data += chunk
-        );
-
-        // The whole response has been received. Print out the result.
-        res.on('end', () => 
-            //console.log(JSON.parse(data))
-            console.log(data)
-        );
-
-    }).on("error", err => 
-        console.log("Error: " + err.message)
-    );*/
+    const obj = JSON.parse(raw);
+    const list = obj.searchResults.listResults;
     
+    for(let i = 0; i < list.length; i++)
+        store.properties.push({...list[i]});
+
+    //now do it again for the rest of the 20 pages
 
     fs.writeFileSync('./data.json', JSON.stringify(store));
+
 }
 
 const resHandler = async res => {
@@ -100,70 +99,3 @@ puppeteer.launch({ headless: false, defaultViewport: null, args:['--start-maximi
     //await browser.close();
     
 });
-    
-
-    /*
-    //next part just clicks through, 
-    //this is what actually saves stuff. 
-    page.on('response', async res => {
-        //exclude walk & transit for now, I guess
-        if(res.url().startsWith('https://www.zillow.com/graphql/') && !res.url().includes('WalkAndTransitScoreQuery')){
-            //console.log(res.url());
-            const json = await res.json();
-            //this right here has a good amount of data
-            //console.log(json.data.property);
-            store.propertie6797631646586%2C"north"%3A43.30453570439518}%2C"mapZoom"%3A12%2C"regionSelection"%3A[{"regionId"%3A38225%2C"regionType"%3A6}]%2C"isMapVisible"%3Atrue%2C"filterState"%3A{"isPreMarketForeclosure"%3A{"value"%3Afalse}%2C"isForSaleForeclosure"%3A{"value"%3Afalse}%2C"isAuction"%3A{"value"%3Afalse}%2C"isNewConstruction"%3A{"value"%3Afalse}%2C"isRecentlySold"%3A{"value"%3Atrue}%2C"isForSaleByOwner"%3A{"value"%3Afalse}%2C"isComingSoon"%3A{"value"%3Afalse}%2C"isPreMarketPreForeclosure"%3A{"value"%3Afalse}%2C"isForSaleByAgent"%3A{"value"%3Afalse}}%2C"isListVisible"%3Atrue}&wants=s.push({...json.data.property});
-        
-        }
-    });
-
-
-    let nextPage = false;
-    
-    while(true){
-        
-        //get all cards on the page
-        //.list-card to get all the property cards
-        const cards = await page.$$('.list-card');
-        //console.log(cards[0]);
-    
-        //potentially randomize order
-        //or just add a wait
-        for(let i = 0; i < cards.length; i++){
-            await cards[i].click();
-            await page.waitForNavigation({waitUntil: 'networkidle0'});
-            await page.waitFor(1000);
-        }
-        
-        //if it's not empty, then there's nothing left
-        //if it's [], then there's more
-        const nextBut = await page.$('a[disabled][title="Next page"]');
-
-
-        nextPage = nextBut !== [];
-
-        if(!nextPage)
-            break;
-
-        await page.waitFor(10000);
-        await page.keyboard.press('Escape');
-        await page.click('a[title="Next page"]');
-        await page.waitForNavigation({waitUntil: 'networkidle0'});
-
-            
-
-
-    }
-
-    
-    await browser.close()
-
-})*/
-
-
-
-//when you load all the sold houses, save the time. 
-//then from then on, use the time filter to save time scraping
-//it's not like sold data changes
-
-
